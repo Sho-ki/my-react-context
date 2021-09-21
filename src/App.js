ed0b7b2650d913bf0ab774787273ad90db758452
sho-ki <shoki0116.highjump@gmail.com>
2021-09-22 00:38:28 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createContext, useContext } from "react";
+
+const LanguageContext = createContext();
 
 function App() {
+  const languages = ["JavaScript", "Python", "Java", "Golang"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <LanguageContext.Provider value={{ languages }}>
+      <div className="background">
+        Background　
+        <p className="languages">
+          Pass languages[JavaScript, Python, Java, Golang] to Container 3
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container1 />
+      </div>
+    </LanguageContext.Provider>
+  );
+}
+
+function Container1() {
+  return (
+    <div className="container1">
+      Container 1
+      <Container2 />
+    </div>
+  );
+}
+
+function Container2() {
+  return (
+    <div className="container2">
+      Container 2
+      <Container3 />
+    </div>
+  );
+}
+
+function Container3() {
+  const ctx = useContext(LanguageContext);
+  console.log(ctx.languages);
+  return (
+    <div className="container3">
+      Container 3
+      <ul className="languages-area">
+        {ctx.languages.map((language, i) => (
+          <li key={i}>{language}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// Props version
+// import "./App.css";
+
+// function App() {
+//   const languages = ["JavaScript", "Python", "Java", "Golang"];
+//   return (
+//     <div className="background">
+//       Background
+//       <p className="languages">
+//         Pass languages[JavaScript, Python, Java, Golang] to Container 3
+//       </p>
+//       {/* languages ->  Container1*/}
+//       <Container1 languages={languages} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function Container1({ languages }) {
+//   return (
+//     <div className="container1">
+//       Container 1
+//       <Container2 languages={languages} />
+//       {/* Container1 ->  Container2 */}
+//     </div>
+//   );
+// }
+
+// function Container2({ languages }) {
+//   return (
+//     <div className="container2">
+//       Container 2
+//       <Container3 languages={languages} />
+//       {/* Container2 ->  Container3 */}
+//     </div>
+//   );
+// }
+
+// function Container3({ languages }) {
+//   return (
+//     <div className="container3">
+//       Container 3
+//       <ul className="languages-area">
+//         {languages.map((language, i) => (
+//           <li key={i}>{language}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
